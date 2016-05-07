150a4c62c8853cb1e8357002d4b068e10f885bc7
Vincent AURAT <vincent.aurat@gmail.com>
2016-05-07 23:04:51 +0200

    last updates before exec it and added some translations in english

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ubuntu-postinstall
 bash script to be run by me soon after ubuntu OS installation.
+
+still not have been tested :-) tomorrow i guess
--- ubuntu-gnome-16.04.sh
+++ ubuntu-gnome-16.04.sh
@@ -1,101 +1,78 @@
 #!/bin/bash
 
-# ne pas installer ubuntu-restricted-extras ubuntu-tweak
-
-# Paper theme : http://www.snwh.org
-sudo add-apt-repository ppa:snwh/pulp
-sudo apt-get update
-sudo apt-get install paper-icon-theme paper-gtk-theme
-
-# pour mise à jour de backintime 1.0.10 à 1.0.24
-sudo add-apt-repository ppa:bit-team/stable
+# do not install ubuntu-restricted-extras ubuntu-tweak
 
 ####################################################################
-#packages to install first:
-sudo apt-get install nfs-common backintime-gnome git preload terminator vim-gnome
+# packages to install first:
 
-# add repository for : glances (non mis à jour au 20130701)
-#sudo add-apt-repository ppa:arnaud-hartmann/glances-stable
-
-#add repository for : get things gnome !
-#sudo add-apt-repository ppa:gtg/ppa
-
-#add repository for : Oracle Java
-sudo add-apt-repository ppa:webupd8team/java
-# non installed (yet)... sudo apt-get install oracle-java7-installer
-# if not set : sudo update-java-alternatives -s java-7-oracle
-# waiting quantal package... sudo apt-get install glances
-
-#add repository for undistract-me
-#sudo add-apt-repository ppa:undistract-me-packagers/daily
+sudo apt-get install nfs-common backintime-gnome git preload terminator vim aptitude
 
 
-########################################################
-#setup indicator sensor
-# sudo sensors-detect est à faire
-sudo add-apt-repository ppa:alexmurray/indicator-sensors
+####################################################################
+#repositories PPA 
 
-# google chrome
-# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-# sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+# pour Paper theme : http://www.snwh.org
+sudo add-apt-repository ppa:snwh/pulp
 
-#add repository for : sublime text
+# for update de backintime 1.1.12 and over
+sudo add-apt-repository ppa:bit-team/stable
+
+# for remmina récent
+sudo add-apt-repository ppa:remmina-ppa-team/remmina-next 
+
+# for banshee 3.0 qu'on attends tous... depuis 2 ans
+sudo add-apt-repository ppa:banshee-team/ppa
+
+# add repository for : sublime text
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
-#prise en charge lecteur en fat64 (exfat)
-sudo add-apt-repository ppa:relan/exfat
+# add repository for : Oracle Java
+sudo add-apt-repository ppa:webupd8team/java
+
+# non installed (yet)... sudo apt-get install oracle-java7-installer
+# if not set : sudo update-java-alternatives -s java-7-oracle
+
+# google chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.chrome.list'
+
+########################################################
 
 #install des packages venant de ppas
 sudo apt-get update
 sudo apt-get upgrade
 
-# sudo apt-get install indicator-sensors
-sudo apt-get install sublime-text
+# themes et icônes
+sudo apt-get install paper-icon-theme paper-gtk-theme
+
+sudo apt-get install sublime-text-installer
+
+# support sysfs exfat
 sudo apt-get install exfat-utils fuse-exfat
-#sudo install gtg
-# sudo apt-get install faenza-icon-theme
-# sudo apt-get install google-chrome-stable
 
 # file sharing supports
 sudo apt-get install davfs2
 
 # essentiels
-sudo apt-get install build-essential vim-doc subversion gitg tig zenity unetbootin gparted most tree ncdu goaccess glogg bleachbit ccze virtualbox openjdk-7-jdk tmux
+sudo apt-get install build-essential vim-doc subversion gitg tig zenity unetbootin gparted most tree ncdu goaccess glogg bleachbit ccze virtualbox openjdk-7-jdk tmux oracle-java8-installer
 
 # playing (large and slow download > 30 min)
 # sudo apt-get install 0ad wesnoth dosbox triplea
 
 # desktop apps in french
-sudo apt-get install gtkhash conky conky-all firefox-locale-fr skype retext meld planner dia calibre kmymoney gramps hyphen-fr libreoffice-help-fr libreoffice-l10n-fr verbiste-gnome mythes-fr python-boto python-pygoocanvas ttf-mscorefonts-installer tesseract-ocr tesseract-ocr-fra tesseract-ocr-ita filezilla klavaro focuswriter mate-icon-theme-faenza
-# accessoire... sudo apt-get gnome-pie
-# obsolete ? sudo apt-get install libreoffice-filter-binfilter
+sudo apt-get install gtkhash conky conky-all firefox-locale-fr skype retext meld planner dia calibre kmymoney gramps hyphen-fr libreoffice-help-fr libreoffice-l10n-fr verbiste-gnome mythes-fr python-boto python-pygoocanvas ttf-mscorefonts-installer tesseract-ocr tesseract-ocr-fra tesseract-ocr-ita filezilla klavaro focuswriter sublime-text-installer google-chrome-stable shutter
 
-#undistract-me (notification for long bash commands)
-#sudo add-apt-repository ppa:undistract-me-packagers/daily
-#sudo apt-get install undistract-me
+# obsolete file format ?... sudo apt-get install libreoffice-filter-binfilter
 
 # network and security utlities
 sudo apt-get install htop iftop ifstat iptraf wireshark tshark arp-scan netspeed nmap netpipe-tcp chkrootkit rkhunter remmina
 
 # multimedia supports
-sudo apt-get install avidemux v4l2ucp audacity isomaster vlc x264 ffmpeg2theora oggvideotools istanbul shotwell hugin pavucontrol shutter darktable gimp gimp-plugin-registry ogmrip transmageddon guvcview wavpack mppenc faac flac vorbis-tools faad lame cheese sound-juicer picard arista milkytracker mypaint geeqie easytag radiotray banshee fotoxx phatch
-
-# To install Sublime Package Control, open Sublime Text 2, press ctrl+` (` is the character next to 1) and paste this:
-#
-# not proved... import urllib2,os; pf='Package Control.sublime-package'; ipp=sublime.installed_packages_path(); os.makedirs(ipp) if not os.path.exists(ipp) else None; urllib2.install_opener(urllib2.build_opener(urllib2.ProxyHandler())); open(os.path.join(ipp,pf),'wb').write(urllib2.urlopen('http://sublime.wbond.net/'+pf.replace(' ','%20')).read()); print 'Please restart Sublime Text to finish installation'
+sudo apt-get install avidemux v4l2ucp audacity isomaster vlc x264 ffmpeg2theora oggvideotools istanbul shotwell hugin pavucontrol darktable gimp ogmrip transmageddon guvcview wavpack mppenc faac flac vorbis-tools faad lame cheese sound-juicer picard arista milkytracker easytag radiotray banshee fotoxx phatch
 
 #----------------------------------------------------------
 #ajout des repos :
 #ppa:webupd8team/themes
-#ppa:webupd8team/java
-#ppa:tualatrix/ppa (ubuntu-tweak)
-#ppa:arnaud-hartmann/glances-stable
-#ppa:nae-team/ppa (extensions nautilus)
 #ppa:shutter/ppa
-#ppa:pmjdebruijn/darktable-release
-#ppa:simonschneegans/testing (gnome-pie)
 #ppa:bit-team/stable (last version of backintime)
-#ppa:gtg/ppa (stable GTG)
 
-# which purpose ?
-#sudo apt-get install lsb-core
